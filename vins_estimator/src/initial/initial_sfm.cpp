6f31bad7b380db29b16726e11a0330f44828d979
iwander-all <58039345+iwander-all@users.noreply.github.com>
2021-05-30 17:14:59 +0800

    Add files via upload

--- vins_estimator/src/initial/initial_sfm.cpp
+++ vins_estimator/src/initial/initial_sfm.cpp
@@ -71,6 +71,36 @@ bool GlobalSFM::solveFrameByPnP(Matrix3d &R_initial, Vector3d &P_initial, int i,
 
 }
 
+//@29/5/2021 to triangulate use right and left
+void GlobalSFM::triangulateLeftAndRight(int frame, Eigen::Matrix<double, 3, 4> &Pose_c1w, vector<SFMFeature> &sfm_f,
+			     Matrix3d ric[], Vector3d tic[]){
+    Eigen::Vector3d t_c1c2 = ric[0].transpose() * (tic[1] - tic[0]);
+    Eigen::Matrix3d R_c1c2 = ric[0].transpose() * ric[1];
+    Eigen::Vector3d t_c2c1 = -R_c1c2.transpose() * t_c1c2;
+    Eigen::Matrix3d R_c2c1 = R_c1c2.transpose();
+    Eigen::Matrix<double, 3, 4> Pose_c2w;
+    Pose_c2w.leftCols<3>() = R_c2c1 * Pose_c1w.leftCols<3>();
+    Pose_c2w.rightCols<1>() = R_c2c1 * Pose_c1w.rightCols<1>() + t_c2c1;
+    
+    for (int j = 0; j < feature_num; j++){
+	Vector2d point0;
+	Vector2d point1;
+	for (int k = 0; k < (int)sfm_f[j].observation.size(); k++){
+	    if (sfm_f[j].observation[k].first == frame){
+		point0 = sfm_f[j].observation[k].second;
+		point1 = sfm_f[j].observation1[k].second;
+		Vector3d point_3d;
+		triangulatePoint(Pose_c1w, Pose_c2w, point0, point1, point_3d);
+		sfm_f[j].state = true;
+		sfm_f[j].position[0] = point_3d(0);
+		sfm_f[j].position[1] = point_3d(1);
+		sfm_f[j].position[2] = point_3d(2);
+		break;
+	    }
+	}
+    }
+}
+
 void GlobalSFM::triangulateTwoFrames(int frame0, Eigen::Matrix<double, 3, 4> &Pose0, 
 									 int frame1, Eigen::Matrix<double, 3, 4> &Pose1,
 									 vector<SFMFeature> &sfm_f)
@@ -116,7 +146,9 @@ void GlobalSFM::triangulateTwoFrames(int frame0, Eigen::Matrix<double, 3, 4> &Po
 // relative_t[i][j]  j_t_ji  (j < i)
 bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
 			  const Matrix3d relative_R, const Vector3d relative_T,
-			  vector<SFMFeature> &sfm_f, map<int, Vector3d> &sfm_tracked_points)
+			  vector<SFMFeature> &sfm_f, map<int, Vector3d> &sfm_tracked_points,
+			  Matrix3d ric[], Vector3d tic[]
+ 			)
 {
 	feature_num = sfm_f.size();
 	//cout << "set 0 and " << l << " as known " << endl;
@@ -172,11 +204,16 @@ bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
 		}
 
 		// triangulate point based on the solve pnp result
-		triangulateTwoFrames(i, Pose[i], frame_num - 1, Pose[frame_num - 1], sfm_f);
+		//@29/5/2021 to triangulate use right and left
+		triangulateLeftAndRight(i, Pose[i], sfm_f, ric, tic);
+		//triangulateTwoFrames(i, Pose[i], frame_num - 1, Pose[frame_num - 1], sfm_f);
 	}
+	
 	//3: triangulate l-----l+1 l+2 ... frame_num -2
-	for (int i = l + 1; i < frame_num - 1; i++)
-		triangulateTwoFrames(l, Pose[l], i, Pose[i], sfm_f);
+	//for (int i = l + 1; i < frame_num - 1; i++){
+	//	triangulateTwoFrames(l, Pose[l], i, Pose[i], sfm_f);
+	//}
+	
 	//4: solve pnp l-1; triangulate l-1 ----- l
 	//             l-2              l-2 ----- l
 	for (int i = l - 1; i >= 0; i--)
@@ -191,9 +228,13 @@ bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
 		c_Quat[i] = c_Rotation[i];
 		Pose[i].block<3, 3>(0, 0) = c_Rotation[i];
 		Pose[i].block<3, 1>(0, 3) = c_Translation[i];
+		
 		//triangulate
-		triangulateTwoFrames(i, Pose[i], l, Pose[l], sfm_f);
+		//@29/5/2021 to triangulate use right and left
+		triangulateLeftAndRight(i, Pose[i], sfm_f, ric, tic);		
+		//triangulateTwoFrames(i, Pose[i], l, Pose[l], sfm_f);
 	}
+	
 	//5: triangulate all other points
 	for (int j = 0; j < feature_num; j++)
 	{
@@ -233,16 +274,36 @@ bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
 	ceres::Problem problem;
 	ceres::LocalParameterization* local_parameterization = new ceres::QuaternionParameterization();
 	//cout << " begin full BA " << endl;
+	
+	//29/5/2021 to reproject feature from i left to j right
+	Eigen::Vector3d t_c1c2 = ric[0].transpose() * (tic[1] - tic[0]);
+	Eigen::Matrix3d R_c1c2 = ric[0].transpose() * ric[1];
+	Eigen::Vector3d t_c2c1 = -R_c1c2.transpose() * t_c1c2;
+	Eigen::Matrix3d R_c2c1 = R_c1c2.transpose();
+	
 	for (int i = 0; i < frame_num; i++)
 	{
+		//29/5/2021 to reproject feature from i left to j right
+		Eigen::Matrix3d R_ci2_l = R_c2c1 * c_Quat[i].toRotationMatrix();
+		Quaterniond q_ci2_l = Quaterniond(R_ci2_l);
+		Eigen::Vector3d t_ci2_l = R_c2c1 * c_Translation[i] + t_c2c1;
+		c_translation[i][0] = t_ci2_l.x();
+		c_translation[i][1] = t_ci2_l.y();
+		c_translation[i][2] = t_ci2_l.z();
+		c_rotation[i][0] = q_ci2_l.w();
+		c_rotation[i][1] = q_ci2_l.x();
+		c_rotation[i][2] = q_ci2_l.y();
+		c_rotation[i][3] = q_ci2_l.z();
+		//So, the meaning of c_translation & c_rotation is the transform from l -> j in right
+	  
 		//double array for ceres
-		c_translation[i][0] = c_Translation[i].x();
-		c_translation[i][1] = c_Translation[i].y();
-		c_translation[i][2] = c_Translation[i].z();
-		c_rotation[i][0] = c_Quat[i].w();
-		c_rotation[i][1] = c_Quat[i].x();
-		c_rotation[i][2] = c_Quat[i].y();
-		c_rotation[i][3] = c_Quat[i].z();
+		//c_translation[i][0] = c_Translation[i].x();
+		//c_translation[i][1] = c_Translation[i].y();
+		//c_translation[i][2] = c_Translation[i].z();
+		//c_rotation[i][0] = c_Quat[i].w();
+		//c_rotation[i][1] = c_Quat[i].x();
+		//c_rotation[i][2] = c_Quat[i].y();
+		//c_rotation[i][3] = c_Quat[i].z();
 		problem.AddParameterBlock(c_rotation[i], 4, local_parameterization);
 		problem.AddParameterBlock(c_translation[i], 3);
 		if (i == l)
@@ -262,12 +323,20 @@ bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
 		for (int j = 0; j < int(sfm_f[i].observation.size()); j++)
 		{
 			int l = sfm_f[i].observation[j].first;
-			ceres::CostFunction* cost_function = ReprojectionError3D::Create(
-												sfm_f[i].observation[j].second.x(),
-												sfm_f[i].observation[j].second.y());
+			//ceres::CostFunction* cost_function = ReprojectionError3D::Create(
+			//			sfm_f[i].observation[j].second.x(),
+			//			sfm_f[i].observation[j].second.y());
 
-    		problem.AddResidualBlock(cost_function, NULL, c_rotation[l], c_translation[l], 
-    								sfm_f[i].position);	 
+			//problem.AddResidualBlock(cost_function, NULL, c_rotation[l], c_translation[l],                 
+			//			sfm_f[i].position);
+			
+			//29/5/2021 to reproject feature from i left to j right
+			ceres::CostFunction* cost_function = ReprojectionError3D::Create(
+						sfm_f[i].observation1[j].second.x(),
+						sfm_f[i].observation1[j].second.y());
+			
+			problem.AddResidualBlock(cost_function, NULL, c_rotation[l], c_translation[l],                 
+						sfm_f[i].position);			
 		}
 
 	}
@@ -289,23 +358,38 @@ bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
 	}
 	for (int i = 0; i < frame_num; i++)
 	{
-		q[i].w() = c_rotation[i][0]; 
-		q[i].x() = c_rotation[i][1]; 
-		q[i].y() = c_rotation[i][2]; 
-		q[i].z() = c_rotation[i][3]; 
+		//29/5/2021 to reproject feature from i left to j right
+		Quaterniond q_ci2_l;
+		q_ci2_l.w() = c_rotation[i][0]; 
+		q_ci2_l.x() = c_rotation[i][1]; 
+		q_ci2_l.y() = c_rotation[i][2]; 
+		q_ci2_l.z() = c_rotation[i][3];
+		q[i] = Quaterniond(R_c1c2) * q_ci2_l;
 		q[i] = q[i].inverse();
+		
+		//q[i].w() = c_rotation[i][0]; 
+		//q[i].x() = c_rotation[i][1]; 
+		//q[i].y() = c_rotation[i][2]; 
+		//q[i].z() = c_rotation[i][3]; 
+		//q[i] = q[i].inverse();
 		//cout << "final  q" << " i " << i <<"  " <<q[i].w() << "  " << q[i].vec().transpose() << endl;
 	}
 	for (int i = 0; i < frame_num; i++)
 	{
-
-		T[i] = -1 * (q[i] * Vector3d(c_translation[i][0], c_translation[i][1], c_translation[i][2]));
+		//T[i] = -1 * (q[i] * Vector3d(c_translation[i][0], c_translation[i][1], c_translation[i][2]));
 		//cout << "final  t" << " i " << i <<"  " << T[i](0) <<"  "<< T[i](1) <<"  "<< T[i](2) << endl;
+		
+		//29/5/2021 to reproject feature from i left to j right
+		T[i] = q[i].toRotationMatrix() * R_c1c2 * t_c2c1 - 
+		       q[i].toRotationMatrix() * R_c1c2 * Vector3d(c_translation[i][0], c_translation[i][1], 
+								   c_translation[i][2]);
+	
 	}
 	for (int i = 0; i < (int)sfm_f.size(); i++)
 	{
 		if(sfm_f[i].state)
-			sfm_tracked_points[sfm_f[i].id] = Vector3d(sfm_f[i].position[0], sfm_f[i].position[1], sfm_f[i].position[2]);
+			sfm_tracked_points[sfm_f[i].id] = Vector3d(sfm_f[i].position[0], sfm_f[i].position[1], 
+								   sfm_f[i].position[2]);
 	}
 	return true;
 
